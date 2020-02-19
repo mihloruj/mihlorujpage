9bb808cd51b82d7b6b0393408ca2c4af6cbfbf86
Ружейников Михаил <56508167+mihloruj@users.noreply.github.com>
2020-02-19 20:57:37 +0300

    Try add my site
    Mb don't work

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    
+    $("#nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 750);
+    });
+
+    
+    $("#btnAlert").click(function() {
+        var text = document.getElementById('txtAlert').value;
+        if (text != "") {
+            alert(text);
+        }
+        else {
+            alert("Введите текст!");
+        };
+    });
+
+    $("#btnStartApp").click(function() {
+        
+    });
+
+    $(window).scroll(function () {
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 50) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+});
