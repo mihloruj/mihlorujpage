6ba64d9ee12f4c973f79f1284089a6cfa39b5e93
Ружейников Михаил <56508167+mihloruj@users.noreply.github.com>
2020-02-27 16:43:10 +0300

    roflo-commit
    amazing bg

--- js/main.js
+++ js/main.js
@@ -18,10 +18,6 @@ $(document).ready(function(){
         };
     });
 
-    $("#btnStartApp").click(function() {
-        
-    });
-
     $(window).scroll(function () {
         // Если отступ сверху больше 50px то показываем кнопку "Наверх"
         if ($(this).scrollTop() > 50) {
@@ -38,5 +34,50 @@ $(document).ready(function(){
         }, 1250);
         return false;
     });
+
+
+    // Калекалятор
+
+    // VARIABLES
+    var calc = $('.calculator');
+    var calcDisplay = calc.find('.calculator__display');
+    var calcKeys = calc.find('.calculator__key');
+    var calcButton = calc.find('.calculator__button');
+    var calcClear = calc.find('.calculator__clear');
+    var calcEqual = calc.find('.calculator__key--equal');
+    var calcPower = calc.find('.calculator__power');
+    var calcSpace = calc.find('.calculator__backspace');
+
+    // INIT CALC KEYS
+    calcKeys.each(function () {
+        var current = $(this).attr('value');
+        $(this).text(current);
+    });
+
+    // ADD NUMBERS TO INPUT
+    calcButton.on('click', function () {
+        calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
+    });
+
+    // CLEAR INPUT
+    calcClear.on('click', function () {
+        calcDisplay.val('');
+    });
+
+    // SHOW RESULT
+    calcEqual.on('click', function () {
+        calcDisplay.val( eval( calcDisplay.val() ) );
+    });
+
+    // POWER BUTTON
+    calcPower.on('click', function () {
+        calcDisplay.val( Math.pow( calcDisplay.val(), 2 ) );
+    });
+
+    // BACKSPACE BUTTON
+    calcSpace.on('click', function () { // http://www.w3schools.com/jsref/jsref_substring.asp
+        calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
+    });
+
 
 });
